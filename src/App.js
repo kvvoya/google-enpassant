49fd0036bf02c97d399c2e4a8baf86498c84ef39
Sikvoyo <suruhakken@gmail.com>
2023-04-15 19:06:32 +0300

    that's all

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
+import enPassantLogo from './img/45vdsbn26opa1.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id='logo_wrapper'>
+        <img src={enPassantLogo} alt="Google EnPassant Logo" className='logo'/>
+        <h1>Wonder how did your pawn just disappear? You're in a right place!</h1>
+        <a href='https://www.google.com/search?q=en+passant'><button>Google En Passant</button></a>
+      </div>
     </div>
   );
 }
